c7d62e91f09e20571d459d6fc58b2766a16afcf8
Sanjay Kumar <kp19nimcet28@gmail.com>
2025-05-31 21:30:53 +0530

    implementation of controller and service

--- src/main/java/org/email/notification/DTO/EmailRequest.java
+++ src/main/java/org/email/notification/DTO/EmailRequest.java
@@ -1,2 +1,19 @@
-package org.email.notification.DTO;public class EmailRequest {
+package org.email.notification.DTO;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+public class EmailRequest {
+    private String to;
+    private String subject;
+    private String body;
+    private List<String> cc;
 }
